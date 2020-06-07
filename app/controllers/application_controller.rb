3900a4142f5bd9ae4d73f7ee3321875c904e94a1
tearoflily <tearoflily1019@gmail.com>
2020-06-07 09:22:53 +0000

    Finish edit-only-views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,16 @@ class ApplicationController < ActionController::Base
   $days_of_the_week = %w{ 日 月 火 水 木 金 土}
   
   def set_one_month
-    @first_day = params[:date].nil? ? Date.current.beginning_of_month: params[:date].to_date
+    @first_day = params[:date].nil? ? Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
-    @one_month = [*@first_day..@last_day]
+    one_month = [*@first_day..@last_day]
     
     @user = User.find(params[:user_id])
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     
-    unless @one_month.count == @attendances.count
+    unless one_month.count == @attendances.count
       ActiveRecord::Base.transaction do
-        @one_month.each do |day|
+        one_month.each do |day|
         @user.attendances.create!(worked_on: day)
         end
       end
